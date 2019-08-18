522356729d6978f9662475910d322cbaf2802280
yunqian <yunqian@pinduoduo.com>
2019-08-18 20:45:59 +0800

    feat: promise

--- src/promise/index.js
+++ src/promise/index.js
@@ -4,18 +4,55 @@ const REJECTED = Symbol("rejected")
 const noop = () => {}
 const isFunction = fn => typeof fn === "function"
 
-const FyberPromise = executor => {
-  let value = null
-  let deffered = []
-  let status = PENDING
+function FyberPromise(executor) {
+  this.value = null
+  this.deffered = []
+  this.status = PENDING
+  this.reason = null
 
-  const resolve = (promise, value) => {}
-  const reject = (promise, reason) => {}
+  const resolve = value => {
+    if (this.status !== PENDING) return
+
+    this.status = FULFILLED
+    this.value = value
+    this.deffered.forEach(item => {
+      this.value = item.onFulfilled(this.value)
+    })
+    this.deffered.length = 0
+  }
+  const reject = reason => {}
 
   executor(resolve, reject)
 }
 
-FyberPromise.prototype.then = (onFulfilled, onRejected) => {
-  if (!isFunction(onFulfilled) || !isFunction(onRejected)) {
-  }
+function Handler(onFulfilled, onRejected, promise) {
+  this.onFulfilled = isFunction(onFulfilled) ? onFulfilled : null
+  this.onRejected = isFunction(onRejected) ? onRejected : null
+  this.promise = promise
 }
+
+FyberPromise.prototype.then = function(onFulfilled, onRejected) {
+  const newPromise = new FyberPromise(() => {})
+  const deffered = new Handler(onFulfilled, onRejected, newPromise)
+  if (this.status === PENDING) {
+    this.deffered.push(deffered)
+    newPromise.deffered = this.deffered
+  }
+  if (this.status === FULFILLED && isFunction(onFulfilled)) {
+    this.value = onFulfilled(this.value)
+  }
+  if (this.status === REJECTED && isFunction(onRejected)) {
+    onRejected(this.reason)
+  }
+  return newPromise
+}
+
+const pros = new FyberPromise((resolve, reject) => {
+  setTimeout(() => resolve(1), 200)
+})
+pros
+  .then(value => {
+    console.log(value, "one")
+    return value
+  })
+  .then(value => console.log(value, "two"))
