713b7748e9b6b1ccdf001977615e3073dd298e06
andyjxli <andyjxli@tencent.com>
2021-07-12 11:41:20 +0800

    fix: hooks

--- webhook.js
+++ webhook.js
@@ -1,27 +1,23 @@
-const http = require('http')
-const createHandler = require('node-github-webhook')
-const handler = createHandler([
-  // 多个仓库
-  {
-    path: '/webhook/blog',
-    secret: 'CUSTOM_blog'
-  }
-])
+import { createServer } from 'http'
+import { spawn } from 'child_process'
+import createHandler from 'github-webhook-handler'
 
-http
-  .createServer((req, res) => {
-    handler(req, res, () => {
-      res.statusCode = 404
-      res.end('no such location')
-    })
+// 下面填写的myscrect跟github webhooks配置一样，下一步会说；path是我们访问的路径
+var handler = createHandler({ path: '/webhook/blog', secret: 'CUSTOM_blog' })
+
+createServer(function(req, res) {
+  handler(req, res, function(err) {
+    res.statusCode = 404
+    res.end('no such location')
   })
-  .listen(4396)
+}).listen(4396)
 
-handler.on('error', err => {
-  console.error('Error:', err)
+handler.on('error', function(err) {
+  console.error('Error:', err.message)
 })
 
-handler.on('push', event => {
+// 监听到push事件的时候执行我们的自动化脚本
+handler.on('push', function(event) {
   console.log(
     'Received a push event for %s to %s',
     event.payload.repository.name,
@@ -29,19 +25,28 @@ handler.on('push', event => {
   )
   const path = event.path.replace('/webhook', '')
   console.log(path, '')
-  // runCmd('sh', [`.${path}/bash.sh`, event.payload.repository.name], (text) => {
-  //   console.log(text);
-  // });
+
+  runCommand('sh', [`.${path}/bash.sh`, event.payload.repository.name], function(txt) {
+    console.log(txt)
+  })
 })
 
-// function runCmd(cmd, args, callback) {
-//   const { spawn } = require('child_process');
-//   const child = spawn(cmd, args);
-//   let resp = '';
-//   child.stdout.on('data', (buffer) => {
-//     resp += buffer.toString();
-//   });
-//   child.stdout.on('end', () => {
-//     callback(resp);
-//   });
-// }
+function runCommand(cmd, args, callback) {
+  var child = spawn(cmd, args)
+  var response = ''
+  child.stdout.on('data', function(buffer) {
+    resp += buffer.toString()
+  })
+  child.stdout.on('end', function() {
+    callback(resp)
+  })
+}
+
+// 由于我们不需要监听issues，所以下面代码注释掉
+//  handler.on('issues', function (event) {
+//    console.log('Received an issue event for %s action=%s: #%d %s',
+//      event.payload.repository.name,
+//      event.payload.action,
+//      event.payload.issue.number,
+//      event.payload.issue.title)
+// });
