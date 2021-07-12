f23c5b52260f05f4616d09e66863426138e6eb38
andyjxli <andyjxli@tencent.com>
2021-07-12 10:44:11 +0800

    fix: webhook

---
+++ webhook.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const createHandler = require('node-github-webhook')
+const handler = createHandler([
+  // 多个仓库
+  {
+    path: '/webhook/blog',
+    secret: 'CUSTOM_blog'
+  }
+])
+
+http
+  .createServer((req, res) => {
+    handler(req, res, () => {
+      res.statusCode = 404
+      res.end('no such location')
+    })
+  })
+  .listen(4396)
+
+handler.on('error', err => {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', event => {
+  console.log(
+    'Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref
+  )
+  const path = event.path.replace('/webhook', '')
+  console.log(path, '')
+  runCmd('sh', [`.${path}/bash.sh`, event.payload.repository.name], text => {
+    console.log(text)
+  })
+})
+
+function runCmd(cmd, args, callback) {
+  const { spawn } = require('child_process')
+  const child = spawn(cmd, args)
+  let resp = ''
+  child.stdout.on('data', buffer => {
+    resp += buffer.toString()
+  })
+  child.stdout.on('end', () => {
+    callback(resp)
+  })
+}
