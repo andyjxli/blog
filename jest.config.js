7db1b8089aac37f7a1c4dd8e5fa4b39e7e4616ec
andyjxli <andyjxli@tencent.com>
2021-07-20 16:29:29 +0800

    feat: init

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  modulePaths: ['<rootDir>/src', '<rootDir/>code>'],
-  testPathIgnorePatterns: ['/node_modules/', '/.cache/']
-}
--- src/utils/link.ts
+++
@@ -1,4 +0,0 @@
-export const linkTo = (path: string) => {
-  if (typeof window === 'undefined') return path
-  return location.href.includes('/localhost') ? path : `/blog/${path}`
-}
